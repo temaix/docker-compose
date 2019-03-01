aa0376252adf10dd5e672cba2478e96012cbe753
Yann <yann.magnabosco@hopps-group.com>
2019-03-01 16:17:58 +0100

    file to init database

--- src/main/java/fr/temaix/RequestPostgres.java
+++ src/main/java/fr/temaix/RequestPostgres.java
@@ -6,21 +6,21 @@ public class RequestPostgres {
 
     public String readVersion() {
 
-//        String url = "jdbc:postgresql://localhost:5432/bdd";
-//        String user = "etraffic";
-//        String password = "etraffic";
-//
-//        try (Connection con = DriverManager.getConnection(url, user, password);
-//             Statement st = con.createStatement();
-//             ResultSet rs = st.executeQuery("SELECT * from sd_types_euas")) {
-//
-//            if (rs.next()) {
-//                return rs.getString(2);
-//            }
-//
-//        } catch (SQLException ex) {
-//            ex.printStackTrace();
-//        }
+        String url = "jdbc:postgresql://localhost:5432/bdd";
+        String user = "etraffic";
+        String password = "etraffic";
+
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery("SELECT * from sd_types_euas")) {
+
+            if (rs.next()) {
+                return rs.getString(2);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
 
         return "0";
     }
