4c598c74c41cb7669ea03f6a7e0cedbccb186019
Yann Magnabosco <yann.magnabosco@gmail.com>
2019-03-07 09:27:51 +0100

    start docker postgres with init.sql

--- src/test/java/fr/temaix/test/RequestPostgresTest.java
+++ src/test/java/fr/temaix/test/RequestPostgresTest.java
@@ -14,27 +14,27 @@ import java.sql.Statement;
 
 public class RequestPostgresTest {
 
-    @BeforeClass
-    public static void init() {
-        String url = "jdbc:postgresql://localhost:5432/bdd";
-        String user = "etraffic";
-        String password = "etraffic";
-        File sqlFile = new File("docker/init.sql");
-        System.out.println(sqlFile.getAbsolutePath());
-
-        try (Connection con = DriverManager.getConnection(url, user, password);
-             Statement st = con.createStatement();
-             BufferedReader in = new BufferedReader(new FileReader(sqlFile))) {
-            String str;
-            StringBuffer sb = new StringBuffer();
-            while ((str = in.readLine()) != null) {
-                sb.append(str + "\n ");
-            }
-            st.executeUpdate(sb.toString());
-        } catch (Exception e) {
-            System.err.println("Failed to Execute. The error is " + e.getMessage());
-        }
-    }
+    //@BeforeClass
+//    public static void init() {
+//        String url = "jdbc:postgresql://10.43.8.90:5432/bdd";
+//        String user = "etraffic";
+//        String password = "etraffic";
+//        File sqlFile = new File("docker/init.sql");
+//        System.out.println(sqlFile.getAbsolutePath());
+//
+//        try (Connection con = DriverManager.getConnection(url, user, password);
+//             Statement st = con.createStatement();
+//             BufferedReader in = new BufferedReader(new FileReader(sqlFile))) {
+//            String str;
+//            StringBuffer sb = new StringBuffer();
+//            while ((str = in.readLine()) != null) {
+//                sb.append(str + "\n ");
+//            }
+//            st.executeUpdate(sb.toString());
+//        } catch (Exception e) {
+//            System.err.println("Failed to Execute. The error is " + e.getMessage());
+//        }
+//    }
 
     @Test
     public void test() {
